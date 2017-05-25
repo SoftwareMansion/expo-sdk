81e31b01e18b1bfd242551f532562be85f24d60f
Alicja Warchal <alicja.warchal@swmansion.com>
2017-05-25 09:40:42 +0200

    Add new fields and constants to Contacts API

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,7 +2,8 @@
 
 import { NativeModules } from 'react-native';
 
-type FieldType = 'phoneNumbers' | 'emails' | 'addresses';
+type FieldType = 'phoneNumbers' | 'emails' | 'addresses' | 'socialProfiles' |
+                 'instantMessageAddresses' | 'urls' | 'dates' | 'relations';
 
 type Options = {
   pageSize?: number,
@@ -12,19 +13,41 @@ type Options = {
 
 type Contact = {
   id: number,
+  contactType: string,
   name: string,
   firstName?: string,
   middleName?: string,
   lastName?: string,
+  previousLastName?: string,
+  namePrefix?: string,
+  nameSuffix?: string,
+  nickname?: string,
+  phoneticFirstName?: string,
+  phoneticMiddleName?: string,
+  phoneticLastName?: string,
+  birthday?: {
+    day?: number,
+    month?: number,
+    year?: number,
+  },
+  nonGregorianBirthday?: {
+    day?: number,
+    month?: number,
+    year?: number,
+  },
   emails?: {
     email?: string,
     primary?: boolean,
     label: string,
+    id: number,
   }[],
   phoneNumbers?: {
     number?: string,
     primary?: boolean,
+    digits?: string,
+    countryCode?: string,
     label: string,
+    id: number,
   }[],
   addresses?: {
     street?: string,
@@ -32,12 +55,56 @@ type Contact = {
     country?: string,
     region?: string,
     neighborhood?: string,
-    postcode?: string,
-    pobox?: string,
+    postalCode?: string,
+    poBox?: string,
+    isoCountryCode?: string,
     label: string,
+    id: number,
+  }[],
+  socialProfiles?: {
+    service?: string,
+    localizedProfile?: string,
+    url?: string,
+    username?: string,
+    userId?: string,
+    label: string,
+    id: number,
+  }[],
+  instantMessageAddresses?: {
+    service?: string,
+    username?: string,
+    localizedService?: string,
+    label: string,
+    id: number,
+  }[],
+  urls?: {
+    label: string,
+    url?: string,
+    id: number,
   }[],
   company?: string,
   jobTitle?: string,
+  department?: string,
+  imageAvailable?: bool,
+  image?: {
+    uri?: string,
+  },
+  thumbnail?: {
+    uri?: string,
+  },
+  note?: string,
+  dates?: {
+    day?: number,
+    month?: number,
+    year?: number,
+    id: number,
+    label: string,
+  }[],
+  relations?: {
+    label: string,
+    name?: string,
+    id: number,
+  }[],
 };
 
 type Response = {
@@ -62,3 +129,18 @@ export async function getContactsAsync(
 export const PHONE_NUMBERS = 'phoneNumbers';
 export const EMAILS = 'emails';
 export const ADDRESSES = 'addresses';
+export const IMAGE = 'image';
+export const THUMBNAIL = 'thumbnail';
+export const NOTE = 'note';
+export const BIRTHDAY = 'birthday';
+export const NON_GREGORIAN_BIRTHDAY = 'nonGregorianBirthday';
+export const NAME_PREFIX = 'namePrefix';
+export const NAME_SUFFIX = 'nameSuffix';
+export const PHONETIC_FIRST_NAME = 'phoneticFirstName';
+export const PHONETIC_MIDDLE_NAME = 'phoneticMiddleName';
+export const PHONETIC_LAST_NAME = 'phoneticLastName';
+export const SOCIAL_PROFILES = 'socialProfiles';
+export const IM_ADDRESSES = 'instantMessageAddresses';
+export const URLS = 'urlAddresses';
+export const DATES = 'dates';
+export const RELATIONS = 'relations';
