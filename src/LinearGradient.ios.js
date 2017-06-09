f1dd58852ef3aef35d7f943873d82928d4fc9dd4
Alicja Warchal <alicja.warchal@swmansion.com>
2017-06-09 11:53:38 +0200

    start/end object API

--- src/LinearGradient.ios.js
+++ src/LinearGradient.ios.js
@@ -11,19 +11,35 @@ import {
 
 export default class LinearGradient extends Component {
   static propTypes = {
-    start: PropTypes.arrayOf(PropTypes.number),
-    end: PropTypes.arrayOf(PropTypes.number),
+    start: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.number),
+        PropTypes.object,
+    ]),
+    end: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.number),
+        PropTypes.object,
+    ]),
     colors: PropTypes.arrayOf(PropTypes.string).isRequired,
     locations: PropTypes.arrayOf(PropTypes.number),
     ...ViewPropTypes,
   };
 
   render() {
-    const { colors, locations, ...otherProps } = this.props;
+    const { colors, locations, start, end, ...otherProps } = this.props;
     if (colors && locations && colors.length !== locations.length) {
       console.warn(
         'LinearGradient colors and locations props should be arrays of the same length'
       );
+    }
+
+    // support for current react-native-linear-gradient api
+    let startProp = start;
+    let endProp = end;
+    if (start && start.x && start.y) {
+        startProp = [start.x, start.y];
+    }
+    if (end && end.x && end.y) {
+        endProp = [end.x, end.y];
     }
 
     return (
@@ -31,6 +47,8 @@ export default class LinearGradient extends Component {
         {...otherProps}
         colors={colors.map(processColor)}
         locations={locations ? locations.slice(0, colors.length) : null}
+        start={startProp}
+        end={endProp}
       />
     );
   }
